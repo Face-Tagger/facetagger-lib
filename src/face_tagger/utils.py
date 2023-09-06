4c470ae5e59413f278e8c835a885f158da7adff6
Sohn Young Jin <sonkim1001@naver.com>
2023-09-07 02:44:38 +0900

    feature: Add a utils module for convenient OpenCV function calls

---
+++ src/face_tagger/utils.py
@@ -0,0 +1,24 @@
+import cv2
+from PIL import Image
+
+
+def resize_image(image, width, height):
+    """
+    Resize the given image to specified width and height.
+
+    :param image: An image to resize.
+    :param width: Width of the resized image.
+    :param height: Height of the resized image.
+    :return: Resized image.
+    """
+    return cv2.resize(image, (width, height))
+
+
+def convert_bgr_to_rgb(image):
+    """
+    Convert the BGR format image to RGB format using OpenCV.
+
+    :param image: An image to convert.
+    :return: RGB image.
+    """
+    return Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
