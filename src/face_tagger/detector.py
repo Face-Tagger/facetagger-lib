4c9ae6e40d4cba280794e59fe1b6ff8a4d77a123
Sohn Young Jin <sonkim1001@naver.com>
2023-09-07 02:44:51 +0900

    refactor: Refinement of each class's functions and logic

--- src/face_tagger/detector.py
+++ src/face_tagger/detector.py
@@ -10,25 +10,35 @@ class Detector:
         """
         Constructor of Detector class.
         """
-
         self.face_detector = MTCNN()
+
+    def detect_face(self, image):
+        """
+        Detects faces in an image.
+
+        :param image: Image to detect faces.
+        :return: Detected faces with their bounding boxes.
+        """
+        # Face detection using MTCNN.
+        return self.face_detector.detect_faces(image)
+
+    def crop(self, image, face):
+        """
+        Crops the face area from the image.
+
+        :param image: Image from which to crop the face.
+        :param face: Detected face with bounding box.
+        :return: Cropped face image.
+        """
+        x, y, width, height = face['box']
+        return image[y:y + height, x:x + width]
 
     def detect_face_and_crop(self, image):
         """
         Detects faces in an image and returns cropped face images.
 
         :param image: Image to detect faces.
-        :return: Cropped face images.
+        :return: List of cropped face images.
         """
-
-        # Face detection using MTCNN.
-        faces = self.face_detector.detect_faces(image)
-
-        # Crop face areas in the image.
-        cropped_faces = []
-        for face in faces:
-            x, y, width, height = face['box']
-            cropped_face = image[y:y + height, x:x + width]
-            cropped_faces.append(cropped_face)
-
-        return cropped_faces
+        faces = self.detect_face(image)
+        return [self.crop(image, face) for face in faces]
--- src/face_tagger/embedder.py
+++ src/face_tagger/embedder.py
@@ -1,8 +1,7 @@
-import cv2
 import torch
 import torchvision.transforms as transforms
-from PIL import Image
 from facenet_pytorch import InceptionResnetV1
+from .utils import resize_image, convert_bgr_to_rgb
 
 
 class Embedder:
@@ -16,10 +15,18 @@ class Embedder:
 
         :param use_gpu: Use GPU or not.
         """
-
         self.device = torch.device('cuda' if use_gpu and torch.cuda.is_available() else 'cpu')
         self.resnet = InceptionResnetV1(pretrained='vggface2').eval().to(self.device)
         self.transform = transforms.ToTensor()
+
+    def prepare_image_tensor(self, face_image):
+        """
+        Convert the image to tensor format and add batch dimension.
+
+        :param face_image: An image to process.
+        :return: Processed image tensor.
+        """
+        return self.transform(face_image).unsqueeze(0).to(self.device)
 
     def compute_embeddings(self, face_image):
         """
@@ -28,15 +35,9 @@ class Embedder:
         :param face_image: An image to compute embeddings.
         :return: Embeddings of an image.
         """
-
-        # Resize the image to 160x160.
-        face_image = cv2.resize(face_image, (160, 160))
-
-        # Convert the image to RGB.
-        face_image = Image.fromarray(cv2.cvtColor(face_image, cv2.COLOR_BGR2RGB))
-
-        # Convert the image to tensor and add a batch dimension.
-        img_tensor = self.transform(face_image).unsqueeze(0).to(self.device)
+        face_image = resize_image(face_image, 160, 160)
+        face_image = convert_bgr_to_rgb(face_image)
+        img_tensor = self.prepare_image_tensor(face_image)
 
         # Compute embeddings.
         img_embedding = self.resnet(img_tensor)
