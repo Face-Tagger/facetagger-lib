293b079c3669a124eb7c7d69c3dd9bcf70eb74ab
Sohn Young Jin <sonkim1001@naver.com>
2023-09-07 02:48:47 +0900

    docs: Update comments

--- src/face_tagger/classifier.py
+++ src/face_tagger/classifier.py
@@ -11,33 +11,36 @@ class Classifier:
         """
         Constructor of Classifier class.
         """
+
         pass
 
     def convert_to_numpy(self, face_embeddings):
         """
         Convert embeddings to numpy format.
-
         :param face_embeddings: Embeddings to convert.
         :return: Numpy format embeddings.
         """
+
         return torch.stack(face_embeddings).view(len(face_embeddings), -1).detach().numpy()
 
     def cluster_embeddings(self, embeddings_np):
         """
         Cluster embeddings using HDBSCAN.
-
         :param embeddings_np: Numpy format embeddings to cluster.
         :return: Cluster labels.
         """
+
         hdbscan_cluster = hdbscan.HDBSCAN(min_cluster_size=2).fit(embeddings_np)
+
         return hdbscan_cluster.labels_
 
     def classify_faces(self, face_embeddings):
         """
         Classify faces using clustering.
-
         :param face_embeddings: Embeddings to classify.
         :return: Cluster labels.
         """
+
         embeddings_np = self.convert_to_numpy(face_embeddings)
+
         return self.cluster_embeddings(embeddings_np)
--- src/face_tagger/embedder.py
+++ src/face_tagger/embedder.py
@@ -12,9 +12,9 @@ class Embedder:
     def __init__(self, use_gpu=False):
         """
         Constructor of Embedder class.
-
         :param use_gpu: Use GPU or not.
         """
+
         self.device = torch.device('cuda' if use_gpu and torch.cuda.is_available() else 'cpu')
         self.resnet = InceptionResnetV1(pretrained='vggface2').eval().to(self.device)
         self.transform = transforms.ToTensor()
@@ -22,19 +22,19 @@ class Embedder:
     def prepare_image_tensor(self, face_image):
         """
         Convert the image to tensor format and add batch dimension.
-
         :param face_image: An image to process.
         :return: Processed image tensor.
         """
+
         return self.transform(face_image).unsqueeze(0).to(self.device)
 
     def compute_embeddings(self, face_image):
         """
         Compute embeddings of an image.
-
         :param face_image: An image to compute embeddings.
         :return: Embeddings of an image.
         """
+
         face_image = resize_image(face_image, 160, 160)
         face_image = convert_bgr_to_rgb(face_image)
         img_tensor = self.prepare_image_tensor(face_image)
